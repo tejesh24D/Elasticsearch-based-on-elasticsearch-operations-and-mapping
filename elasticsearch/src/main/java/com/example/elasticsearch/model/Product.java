54efcb959cf504fb5c692fc04d3e20d8948bda56
tejesh24D <tejesh@recruitly.io>
2024-09-13 17:36:00 +0530

    Add files via upload

---
+++ elasticsearch/src/main/java/com/example/elasticsearch/model/Product.java
@@ -0,0 +1,65 @@
+package com.example.elasticsearch.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "products")
+public class Product {
+
+    @Id
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Double)
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ elasticsearch/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+<h1>Welcome to the Elasticsearch Application!</h1>
+<p><a href="/products">Go to Products</a></p>
+<p><a href="/index-setup">Setup Elasticsearch Index</a></p>
+</body>
+</html>
